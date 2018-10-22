9a1d8109bb52fa6d8ae6bede0a7e547e3f08198a
Gitesh <wadhwagitesh2@gmail.com>
2018-10-22 22:32:47 +0530

    changes in minimum braacket reversal code

--- minimum bracket reversal in cpp.cpp
+++ minimum bracket reversal in cpp.cpp
@@ -1,32 +1,46 @@
 #include<stack>
 #include<cstring>
-int countBracketReversals(char input[]){
-	// Write your code here
-int len=strlen(input);
-  if(len%2!=0)
-  {
-    return -1;
-  }
-   stack<char> s;
-    for (int i=0; i<len; i++)
+#include<iostream>
+using namespace std;
+int reversals(char st[])
+{
+    int len=strlen(st);
+    if(len%2!=0)
     {
-        if (input[i]=='}' && !s.empty())
-        {
-            if (s.top()=='{')
-                s.pop();
-            else
-                s.push(input[i]);
-        }
-        else
-            s.push(input[i]);
+
+        return -1;
     }
-    int Or_len = s.size();
+    stack<int> s;
+    for(int i=0;i<len;i++)
+    {
+        if(st[i]=='}'&& !s.empty())
+        {
+            if(s.top()=='{')
+                {
+                    s.pop();
+                }else{
+                s.push(st[i]);
+                }
+        }
+        else{
+           s.push(st[i]);
+        }
+    }
+     int Or_len = s.size();
     int n = 0;
     while (!s.empty() && s.top() == '{')
     {
         s.pop();
         n++;
     }
+     return (Or_len/2 + n%2);
 
-    return (Or_len/2 + n%2);
+}
+int main()
+{
+
+    char str[100];
+    cin.getline(str,100);
+    int x=reversals(str);
+   cout<<x;
 }
