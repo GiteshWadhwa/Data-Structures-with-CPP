6bfd89a06918e99aca3e15d7a37535e27d5a06b5
Gitesh <wadhwagitesh2@gmail.com>
2018-10-23 18:07:38 +0530

    taking input levelwise

--- Tree01.cpp
+++ Tree01.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
+#include<queue>
+#include<stack>
 using namespace std;
 #include "Tree01.h"
+TreeNode<int>*takeInputLevelWise()
+{
+    int rootData;
+    cout<<"enter root data:";
+    cin>>rootData;
+    TreeNode<int>* root=new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while(pendingNodes.size()!=0)
+    {
+      TreeNode<int>* front=pendingNodes.front();
+      pendingNodes.pop();
+      cout<<"enter the number of children of"<<front->data<<endl;
+      int numChild;
+      cin>>numChild;
+      for(int i=0;i<numChild;i++)
+      {
+               int childData;
+               cout<<"enter"<<i<<"th child of "<<front->data<<endl;
+               cin>>childData;
+               TreeNode<int>* child=new TreeNode<int>(childData);
+               front->children.push_back(child);
+               pendingNodes.push(child);
+      }
+    }
+    return root;
+}
+
 
 TreeNode<int>* takeInput(){
 int rootData;
 cout<<"data:";
 cin>>rootData;
 TreeNode<int>* root=new TreeNode<int>(rootData);
-
 int n;
 cout<<"enter number of childrens:";
 cin>>n;
@@ -47,7 +78,7 @@ int main()
       TreeNode<int>* node2=new TreeNode<int>(3);
       root->children.push_back(node1);
       root->children.push_back(node2);*/
-      TreeNode<int>* root=takeInput();
+      TreeNode<int>* root=takeInputLevelWise();
 printTree(root);
 //TODO delete the tree
 
