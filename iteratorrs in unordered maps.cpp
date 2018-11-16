07cea91954a4cd40fd2e29aa20fceaff4f1185e9
Gitesh <wadhwagitesh2@gmail.com>
2018-11-16 14:29:12 +0530

    iterators in unordered maps and vectors in cpp

--- iteratorrs in unordered maps.cpp
+++ iteratorrs in unordered maps.cpp
@@ -17,4 +17,17 @@ while(it!=ourmap.end()){
     cout<<"key: "<<it->first<<"Value: "<<it->second<<endl;
     it++;
 }
+
+vector<int> v;
+v.push_back(1);
+v.push_back(2);
+v.push_back(3);
+v.push_back(4);
+v.push_back(5);
+
+vector<int>::iterator it1=v.begin();
+while(it1!=v.end()){
+    cout<<*it1<<endl;
+  it1++;
+}
 }
